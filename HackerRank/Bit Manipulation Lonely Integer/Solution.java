a700cc97e634fb37caeef449bcd3aa61f6896443
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-01 10:33:39 -0500

    Bit Maniulation Lonely Integer solution

---
+++ HackerRank/Bit Manipulation Lonely Integer/Solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        int xor = 0;
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+            xor^=a[a_i];
+        }
+        System.out.println(xor);
+    }
+}
