bd5eee94f0710e52b9c361ef546f588ccc0ddabf
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-02 05:13:35 -0500

    27. Remove Element solution

---
+++ LeetCode/27. Remove Element/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int newCount=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[newCount++]=nums[i];
+            }
+        }
+        return newCount;
+    }
+}
