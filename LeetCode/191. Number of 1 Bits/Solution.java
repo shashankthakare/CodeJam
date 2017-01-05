ed6bea2939b7059f467ff4ec45353263203c9b28
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-05 18:28:04 -0500

    191. Better java solution without use of built in API

--- LeetCode/191. Number of 1 Bits/Solution.java
+++ LeetCode/191. Number of 1 Bits/Solution.java
@@ -1,6 +1,12 @@
 public class Solution {
-    // you need to treat n as an unsigned value
+    
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        int count=0;
+        for(int i=0;i<32;i++){
+         
+         count += (n>>i&1);
+         
+        }
+        return count;
     }
 }
