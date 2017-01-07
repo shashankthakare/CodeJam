634c88acb038f7556dc527a04bae0f206b764ad0
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-07 16:32:21 -0500

    Hash Tables Ransom Note partial solution
    todo: handle cases for word repetition

---
+++ HackerRank/Hash Tables Ransom Note/Solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+//todo: handle cases for word repetition
+public class Solution {
+    HashMap<String, Integer> magazineMap = new HashMap<String, Integer>();
+    
+    public boolean solve(String magazine, String note) {
+        
+        String[] magazineParts = magazine.split(" ");
+        String[] noteParts = note.split(" ");
+        
+        for(int i=0; i<magazineParts.length; i++){
+            magazineMap.put(magazineParts[i],i);
+        }
+        
+        for(int i=0; i<noteParts.length; i++){
+            if(magazineMap.containsKey(noteParts[i])==false)
+                return false;
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        
+        // Eat whitespace to beginning of next line
+        scanner.nextLine();
+        String magazine, note;
+        
+        Solution s = new Solution();
+        
+        magazine = scanner.nextLine();
+        note = scanner.nextLine();
+        
+        scanner.close();
+        
+        boolean answer = s.solve(magazine,note);
+        if(answer)
+            System.out.println("Yes");
+        else System.out.println("No");
+      
+    }
+}
