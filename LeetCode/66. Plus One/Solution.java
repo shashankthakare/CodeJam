9d1fbde11c38e6f454b47e51464f333dff1c62d5
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-07 18:16:09 -0500

    66: handled base case for array bound exception

--- LeetCode/66. Plus One/Solution.java
+++ LeetCode/66. Plus One/Solution.java
@@ -1,6 +1,12 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         
+        if(digits[0]==0){
+            int[] base = new int[1];
+            base[0]=1;
+            return base;
+        } 
+        
         int sum = 0;
         
         for(int i=0;i<digits.length;i++){
