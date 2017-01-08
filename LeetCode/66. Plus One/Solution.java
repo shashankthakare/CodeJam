1f71bb70634f34dcdae9679c22317314187d832b
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-07 19:00:58 -0500

    66. Plus One
    In place processing to handle large inputs where digits>
    Integer.MAX_VALUE

--- LeetCode/66. Plus One/Solution.java
+++ LeetCode/66. Plus One/Solution.java
@@ -1,28 +1,31 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         
+        int len = digits.length-1;
+        
+        for(int i = len; i>=0; i--){
+            if(digits[i]==9){
+            
+                digits[i]=0;
+                 
+            }
+            else{
+                
+                digits[i]++;
+                break;
+                
+            }
+            
+        }
+       
         if(digits[0]==0){
-            int[] base = new int[1];
-            base[0]=1;
-            return base;
-        } 
-        
-        int sum = 0;
-        
-        for(int i=0;i<digits.length;i++){
-            sum = sum*10 + digits[i];
-        }
-        
-        sum+=1;
-        
-        int length = Integer.toString(sum).length();
-        int[] result = new int[length];
-        
-        while(sum>0)
-        {
-            result[--length] = sum%10;
-            sum/=10;
-        }
-        return result;
+            
+                int[] biggerNumber = new int[digits.length+1];
+                biggerNumber[0]=1;
+                return biggerNumber;
+                
+            }
+            
+        return digits;
     }
 }
