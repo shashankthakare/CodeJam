47ff9e67177bb26c6b9fda7e63728bd9e31b8e20
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-10 22:45:42 -0500

    WCF test suite

---
+++ Practice/wcf/TestSuite/ClientExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace TestSuite
+{
+    public static class EmployeeManagementClientExtension
+    {
+        public static void CloseClient(this ICommunicationObject client)
+        {
+            if (client == null)
+                return;
+            if (client.State == CommunicationState.Faulted)
+                client.Abort();
+            else
+                client.Close();
+                      
+        }
+
+
+
+    }
+}
---
+++ Practice/wcf/TestSuite/CreateEmployeeRequestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestSuite
+{
+    public static class CreateEmployeeRequestExtension
+    {
+        public static void Validate(this CreateEmployeeRequest request)
+        {
+            if (request == null)
+                return;
+        }
+    }
+}
