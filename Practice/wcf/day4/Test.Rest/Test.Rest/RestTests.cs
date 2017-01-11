1c658bf36ebc77e5f86c5c1bd1275a08303c467a
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-10 22:48:00 -0500

    WCF Day 4

---
+++ Practice/wcf/day4/Test.Rest/Test.Rest/RestTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+
+namespace Test.Rest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class RestTests
+    {
+        public RestTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestGetEmployee()
+        {
+            HttpWebRequest request = WebRequest.Create("http://localhost:7763/EmployeeManagement.svc/employee/100") as HttpWebRequest;
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            
+            StreamReader reader =  new StreamReader(response.GetResponseStream());
+            string strResponse = reader.ReadToEnd();
+            
+
+        }
+    }
+}
---
+++ Practice/wcf/day4/WCF.Rest/WCF.Rest/WCF.Rest/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace WCF.Rest
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
