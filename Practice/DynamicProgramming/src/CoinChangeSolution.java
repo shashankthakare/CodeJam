40a46cca2b6115785302fdbad2534e2a5beea0ec
Shashank Thakare <shashankthakare1@gmail.com>
2017-01-11 22:10:29 -0500

    DP Practice : coinChange solution

---
+++ Practice/DynamicProgramming/src/CoinChangeSolution.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class CoinChangeSolution {
+
+	public int coinChange(int[] coins, int amount) {
+    	int[] solnArray = new int[amount+1];
+    	Arrays.fill(solnArray, (Integer.MAX_VALUE-1));
+    	solnArray[0]=0;
+    	
+    	for(int j=0; j<coins.length; j++){
+    		
+    		for(int i=1; i <= amount; i++){
+    			
+    			if(i>=coins[j]){
+    				solnArray[i] = Math.min(solnArray[i], 1+(solnArray[i- coins[j]]));
+    			}
+    			
+    		}
+    		
+    	}
+    	if(solnArray[amount]!=(Integer.MAX_VALUE-1))
+    		return solnArray[amount];
+    	else
+    		return -1;
+    }
+    
+    public static void main(String[] args){
+    	int[] coins = {1,2,5};
+    	int amount = 11;
+    	CoinChangeSolution sol= new CoinChangeSolution();
+    	System.out.println("Minimum steps are "+sol.coinChange(coins, amount));
+    }
+}
