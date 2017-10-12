ef911018a52c8a175d988747320b29cfc4a864e9
Kanav bhagat <kanav16046@iiitd.ac.in>
2017-10-13 04:30:40 +0530

    Update Solution.java

--- HackerRank/Bit Manipulation Lonely Integer/Solution.java
+++ HackerRank/Bit Manipulation Lonely Integer/Solution.java
@@ -7,14 +7,27 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int a[] = new int[n];
-        int xor = 0;
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
-            xor^=a[a_i];
+Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+        	arr[i]=in.nextInt();
         }
-        System.out.println(xor);
+        int flag=0;
+        for(int i=0;i<n;i++){
+        	int k=arr[i];
+        	for(int j=i+1;j<n;j++){
+        		if(arr[j]==k){
+        			arr[j]=-1;
+        			arr[i]=-1;
+        			break;
+        		}
+        	}
+        }
+        for(int i=0;i<n;i++){
+        	if(arr[i]!=-1){
+        		System.out.println(arr[i]);
+        	}
+        }
     }
 }
